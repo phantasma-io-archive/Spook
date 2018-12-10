3661c99548c70a8706ba5ba69602bb8d33ed958b
Sergio Flores <relfos@gmail.com>
2018-12-10 23:39:34 +0000

    Implemented latest changes in P2P protocol

--- PhantasmaNode/Node.cs
+++ PhantasmaNode/Node.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Phantasma.Cryptography;
 using Phantasma.Network.P2P;
 using Phantasma.Core;
@@ -8,13 +7,39 @@ using System.Net.Sockets;
 using System.Linq;
 using System.Net;
 using System;
-using System.IO;
 using Phantasma.Network.P2P.Messages;
+using System.Threading.Tasks;
+using Phantasma.Numerics;
 
 namespace Phantasma.Blockchain.Consensus
 {
+    public enum EndpointStatus
+    {
+        Waiting,
+        Disabled,
+        Connected,
+    }
+
+    public class EndpointEntry
+    {
+        public readonly Endpoint endpoint;
+        public DateTime lastPing;
+        public int pingDelay;
+        public EndpointStatus status;
+
+        public EndpointEntry(Endpoint endpoint)
+        {
+            this.endpoint = endpoint;
+            this.lastPing = DateTime.UtcNow;
+            this.pingDelay = 32;
+            this.status = EndpointStatus.Waiting;
+        }
+    }
+
     public sealed partial class Node: Runnable
     {
+        public readonly static int MaxActiveConnections = 64;
+
         public readonly int Port;
         public Address Address => keys.Address;
 
@@ -30,8 +55,7 @@ namespace Phantasma.Blockchain.Consensus
         private TcpClient client;
         private TcpListener listener;
 
-        private List<Endpoint> _activeSeeds = new List<Endpoint>();
-        private List<Endpoint> _disabledSeeds = new List<Endpoint>();
+        private List<EndpointEntry> _knownEndpoints = new List<EndpointEntry>();
 
         private bool listening = false;
 
@@ -47,13 +71,32 @@ namespace Phantasma.Blockchain.Consensus
 
             this._mempool = new Mempool(keys, nexus);
 
-            this._activeSeeds = seeds.Select(x => ParseEndpoint(x)).ToList();
+            QueueEndpoints(seeds.Select(seed => ParseEndpoint(seed)));
 
             // TODO this is a security issue, later change this to be configurable and default to localhost
             var bindAddress = IPAddress.Any;
 
             listener = new TcpListener(bindAddress, port);
             client = new TcpClient();
+        }
+
+        private void QueueEndpoints(IEnumerable<Endpoint> endpoints)
+        {
+            Throw.IfNull(endpoints, nameof(endpoints));
+
+            if (!endpoints.Any())
+            {
+                return;
+            }
+
+            lock (_knownEndpoints)
+            {
+                foreach (var endpoint in endpoints)
+                {
+                    var entry = new EndpointEntry(endpoint);
+                    _knownEndpoints.Add(entry);
+                }
+            }
         }
 
         public Endpoint ParseEndpoint(string src)
@@ -72,22 +115,45 @@ namespace Phantasma.Blockchain.Consensus
                 port = this.Port;
             }
 
-            return new Endpoint(src, port);
+            IPAddress ipAddress;
+
+            if (!IPAddress.TryParse(src, out ipAddress))
+            {
+                if (Socket.OSSupportsIPv6)
+                {
+                    if (src == "localhost")
+                    {
+                        ipAddress = IPAddress.IPv6Loopback;
+                    }
+                    else
+                    {
+                        ipAddress = Endpoint.ResolveAddress(src, AddressFamily.InterNetworkV6);
+                    }
+                }
+                if (ipAddress == null)
+                {
+                    ipAddress = Endpoint.ResolveAddress(src, AddressFamily.InterNetwork);
+                }
+            }
+
+            if (ipAddress == null)
+            {
+                throw new Exception("Invalid address: " + src);
+            }
+            else
+            {
+                src = ipAddress.ToString();
+            }
+
+            return new Endpoint(PeerProtocol.TCP, src, port);
         }
 
         protected override bool Run()
         {
-            lock (_peers)
-            {
-                if (ConnectToSeeds())
-                {
-                    return true;
-                }
-            }
+            ConnectToPeers();
 
             if (!listening)
             {
-                Log.Debug("Waiting for new connections");
                 listening = true;
                 var accept = listener.BeginAcceptSocket(new AsyncCallback(DoAcceptSocketCallback), listener);
             }
@@ -107,34 +173,103 @@ namespace Phantasma.Blockchain.Consensus
             listener.Stop();
         }
 
-        private bool ConnectToSeeds()
+        private bool IsKnown(Endpoint endpoint)
         {
-            if (_peers.Count == 0 && _activeSeeds.Count > 0)
+            lock (_peers)
             {
-                var idx = Environment.TickCount % _activeSeeds.Count;
-                var target = _activeSeeds[idx];
-
-                var result = client.BeginConnect(target.Host, target.Port, null, null);
-
-                var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
-
-                if (!success)
+                foreach (var peer in _peers)
                 {
-                    Log.Message("Could not reach seed " + target);
-                    _disabledSeeds.Add(target);
-                    _activeSeeds.RemoveAt(idx);
+                    if (peer.Endpoint.Equals(endpoint))
+                    {
+                        return true;
+                    }
                 }
-                else
-                {
-                    Log.Message("Connected to seed " + target);
-                    client.EndConnect(result);
+            }
 
-                    HandleConnection(client.Client, true);
-                    return true;
+            lock (_knownEndpoints)
+            {
+                foreach (var peer in _knownEndpoints)
+                {
+                    if (peer.endpoint.Equals(endpoint))
+                    {
+                        return true;
+                    }
                 }
             }
 
             return false;
+        }
+
+        private void ConnectToPeers()
+        {
+            lock (_peers)
+            {
+                if (_peers.Count >= MaxActiveConnections)
+                {
+                    return;
+                }
+            }
+
+            lock (_knownEndpoints)
+            {
+                _knownEndpoints.RemoveAll(x => x.endpoint.Protocol != PeerProtocol.TCP);
+
+                var possibleTargets = new List<int>();
+                for (int i=0; i<_knownEndpoints.Count; i++)
+                {
+                    if (_knownEndpoints[i].status == EndpointStatus.Waiting)
+                    {
+                        possibleTargets.Add(i);
+                    }
+                }
+
+                if (possibleTargets.Count > 0)
+                {
+                    // adds a bit of pseudo randomness to connection order
+                    var idx = Environment.TickCount % possibleTargets.Count;
+                    idx = possibleTargets[idx];
+                    var target = _knownEndpoints[idx];
+
+                    var result = client.BeginConnect(target.endpoint.Host, target.endpoint.Port, null, null);
+
+                    var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+
+                    if (!success)
+                    {
+                        Log.Message("Could not reach peer: " + target.endpoint);
+                        target.status = EndpointStatus.Disabled;
+                        return;
+                    }
+                    else
+                    {
+                        Log.Message("Connected to peer: " + target.endpoint);
+                        client.EndConnect(result);
+                        target.status = EndpointStatus.Connected;
+
+                        Task.Run(() => { HandleConnection(client.Client); });
+                        return;
+                    }
+                }
+            }
+
+            var disabledConnections = _knownEndpoints.Where(x => x.status == EndpointStatus.Disabled);
+            if (disabledConnections.Any())
+            {
+                lock (_knownEndpoints)
+                {
+                    var currentTime = DateTime.UtcNow;
+                    foreach (var entry in disabledConnections)
+                    {
+                        var diff = currentTime - entry.lastPing;
+                        if (diff.TotalSeconds >= entry.pingDelay)
+                        {
+                            entry.lastPing = currentTime;
+                            entry.pingDelay *= 2;
+                            entry.status = EndpointStatus.Waiting;
+                        }
+                    }
+                }
+            }
         }
 
         // Process the client connection.
@@ -143,25 +278,36 @@ namespace Phantasma.Blockchain.Consensus
             listening = false;
 
             // Get the listener that handles the client request.
-            TcpListener listener = (TcpListener)ar.AsyncState;
+            var listener = (TcpListener)ar.AsyncState;
 
             Socket socket = listener.EndAcceptSocket(ar);
             Log.Message("New connection accepted from " + socket.RemoteEndPoint.ToString());
 
-            HandleConnection(socket, false);
+            Task.Run(() => { HandleConnection(socket); });
         }
 
-        private void HandleConnection(Socket socket, bool sendIdentity)
+        private void SendMessage(Peer peer, Message msg)
+        {
+            Throw.IfNull(peer, nameof(peer));
+            Throw.IfNull(msg, nameof(msg));
+
+            Log.Message("Sending "+msg.GetType().Name+" to  " + peer.Endpoint);
+
+            msg.Sign(this.keys);
+            peer.Send(msg);
+        }
+
+        private void HandleConnection(Socket socket)
         {
             var peer = new TCPPeer(Nexus, socket);
-            _peers.Add(peer);
-
-            if (sendIdentity)
+            lock (_peers)
             {
-                var msg = new PeerIdentityMessage(this.Nexus, this.Address);
-                peer.Send(msg);
-                msg.Sign(this.keys);
+                _peers.Add(peer);
             }
+
+            // this initial message is not only used to fetch chains but also to verify identity of peers
+            var request = new RequestMessage(RequestKind.Chains | RequestKind.Peers | RequestKind.Mempool, this.Address);
+            SendMessage(peer, request);
 
             while (true)
             {
@@ -172,41 +318,103 @@ namespace Phantasma.Blockchain.Consensus
                 }
 
                 Console.WriteLine("Got: " + msg.GetType().Name);
+                Console.WriteLine("From: " + msg.Address.Text);
+                foreach (var line in msg.GetDescription())
+                {
+                    Console.WriteLine(line);
+                }
 
                 var answer = HandleMessage(peer, msg);
                 if (answer != null)
                 {
-                    answer.Sign(this.keys);
-                    peer.Send(answer);
+                    SendMessage(peer, answer);
                 }
 
             }
 
+            Log.Message("Disconnected from peer: " + peer.Endpoint);
+
             socket.Close();
 
-            // TODO remove peer from list
+            lock (_peers)
+            {
+                _peers.Remove(peer);
+            }
         }
 
         private Message HandleMessage(Peer peer, Message msg)
         {
-            switch (msg.Opcode) {
+            if (msg.IsSigned && msg.Address != Address.Null)
+            {
+                peer.SetAddress(msg.Address);
+            }
+            else
+            {
+                return new ErrorMessage(Address, P2PError.MessageShouldBeSigned);
+            }
 
-                case Opcode.PEER_Identity:
+            switch (msg.Opcode) {
+                case Opcode.REQUEST:
                     {
-                        // TODO add peer to list and send him list of peers
-                        if (msg.IsSigned && msg.Address != Address.Null)
+                        var request = (RequestMessage)msg;
+
+                        if (request.Kind == RequestKind.None)
                         {
-                            peer.SetAddress(msg.Address);
-                            return null;
+                            return new ErrorMessage(Address, P2PError.InvalidRequest);
                         }
-                        else {
-                            return new ErrorMessage(Nexus, Address, P2PError.MessageShouldBeSigned);
+
+                        var answer = new ListMessage(this.Address, request.Kind);
+
+                        if (request.Kind.HasFlag(RequestKind.Peers))
+                        {
+                            answer.SetPeers(this.Peers.Where(x => x != peer).Select(x => x.Endpoint));
                         }
+
+                        if (request.Kind.HasFlag(RequestKind.Chains))
+                        {
+                            var chains = Nexus.Chains.Select(x => new ChainInfo(x.Name, x.ParentChain != null ? x.ParentChain.Name: "", x.LastBlock != null ? x.LastBlock.Height : 0));
+                            answer.SetChains(chains);
+                        }
+
+                        if (request.Kind.HasFlag(RequestKind.Mempool))
+                        {
+                            var txs = _mempool.GetTransactions().Select(x => Base16.Encode(x.ToByteArray(true)));
+                            answer.SetMempool(txs);
+                        }
+
+                        return answer;
                     }
 
-                case Opcode.PEER_List:
+                case Opcode.LIST:
                     {
-                        // TODO check for any unknown peer and add to the list
+                        var listMsg = (ListMessage)msg;
+
+                        if (listMsg.Kind.HasFlag(RequestKind.Peers))
+                        {
+                            var newPeers = listMsg.Peers.Where(x => !IsKnown(x));
+                            foreach (var entry in listMsg.Peers)
+                            {
+                                Log.Message("New peer: " + entry.ToString());
+                            }
+                            QueueEndpoints(newPeers);
+                        }
+
+                        if (listMsg.Kind.HasFlag(RequestKind.Mempool))
+                        {
+                            int submittedCount = 0;
+                            foreach (var txStr in listMsg.Mempool)
+                            {
+                                var bytes = Base16.Decode(txStr);
+                                var tx = Transaction.Unserialize(bytes);
+                                if (this._mempool.Submit(tx))
+                                {
+                                    submittedCount++;
+                                }
+
+                                Log.Message(submittedCount+" new transactions");
+                            }
+                        }
+
                         break;
                     }
 
@@ -215,18 +423,7 @@ namespace Phantasma.Blockchain.Consensus
                         break;
                     }
 
-                case Opcode.MEMPOOL_List:
-                    {
-                        var txs = _mempool.GetTransactions();
-                        break;
-                    }
-
                 case Opcode.BLOCKS_List:
-                    {
-                        break;
-                    }
-
-                case Opcode.CHAIN_List:
                     {
                         break;
                     }
