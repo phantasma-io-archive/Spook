372e69532264596f622c854bcaaf893241ce6ebf
Sergio Flores <relfos@gmail.com>
2018-12-27 19:34:21 +0000

    TPSplugin moved to its own file

---
+++ PhantasmaCLI/TPSPlugin.cs
@@ -0,0 +1,36 @@
+﻿using Phantasma.Blockchain;
+using Phantasma.Core.Log;
+using System;
+
+namespace Phantasma.CLI
+{
+    public class TPSPlugin : IChainPlugin
+    {
+        private int periodInSeconds;
+        private int txCount;
+        private DateTime lastTime = DateTime.UtcNow;
+        private Logger log;
+
+        public TPSPlugin(Logger log, int periodInSeconds)
+        {
+            this.log = log;
+            this.periodInSeconds = periodInSeconds;
+        }
+
+        public override void OnTransaction(Chain chain, Block block, Transaction transaction)
+        {
+            txCount++;
+
+            var currentTime = DateTime.UtcNow;
+            var diff = (currentTime - lastTime).TotalSeconds;
+
+            if (diff >= periodInSeconds)
+            {
+                lastTime = currentTime;
+                var tps = txCount / (float)periodInSeconds;
+                log.Message($"{tps.ToString("0.##")} TPS");
+                txCount = 0;
+            }
+        }
+    }
+}
