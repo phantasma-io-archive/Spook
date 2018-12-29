d061439621315c7bced68eb19587c0e6c79e04ed
Sergio Flores <relfos@gmail.com>
2018-12-29 16:07:46 +0000

    Updated script files

--- test_main_node.bat
+++ test_main_node.bat
@@ -1,2 +1,2 @@
-dotnet %~dp0PhantasmaCLI/bin/Debug/netcoreapp2.0/Phantasma.CLI.dll -node.wif=L2LGgkZAdupN2ee8Rs6hpkc65zaGcLbxhbSDGq8oh6umUxxzeW25 -rpc.enabled=true
+dotnet %~dp0Spook.CLI/bin/Debug/netcoreapp2.0/Spook.dll -node.wif=L2LGgkZAdupN2ee8Rs6hpkc65zaGcLbxhbSDGq8oh6umUxxzeW25 -rpc.enabled=true
 PAUSE
--- test_sender.bat
+++ test_sender.bat
@@ -1,2 +1,2 @@
-dotnet %~dp0PhantasmaCLI/bin/Debug/netcoreapp2.0/Phantasma.CLI.dll -node.wif=L2LGgkZAdupN2ee8Rs6hpkc65zaGcLbxhbSDGq8oh6umUxxzeW25 -node.mode=sender -sender.host=http://localhost/rpc:7077 -sender.threads=500 -gui.enabled=false
+dotnet %~dp0Spook.CLI/bin/Debug/netcoreapp2.0/Spook.dll -node.wif=L2LGgkZAdupN2ee8Rs6hpkc65zaGcLbxhbSDGq8oh6umUxxzeW25 -node.mode=sender -sender.host=http://localhost/rpc:7077 -sender.threads=500 -gui.enabled=false
 PAUSE
