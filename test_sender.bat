1ebf631ecb9d9137048e82ab79d47d7764bd1855
Sergio Flores <relfos@gmail.com>
2019-01-14 20:46:36 +0000

    Optimized sender.bat

--- test_sender.bat
+++ test_sender.bat
@@ -1,2 +1,2 @@
-dotnet %~dp0Spook.CLI/bin/Debug/netcoreapp2.0/Spook.dll -node.wif=L2LGgkZAdupN2ee8Rs6hpkc65zaGcLbxhbSDGq8oh6umUxxzeW25 -node.mode=sender -sender.host=http://localhost:7077/rpc -sender.threads=500 -gui.enabled=false
+dotnet %~dp0Spook.CLI/bin/Debug/netcoreapp2.0/Spook.dll -node.wif=L2LGgkZAdupN2ee8Rs6hpkc65zaGcLbxhbSDGq8oh6umUxxzeW25 -node.mode=sender -sender.host=http://localhost:7077/rpc -sender.threads=20 -gui.enabled=false -sender.addressCount=6000
 PAUSE
